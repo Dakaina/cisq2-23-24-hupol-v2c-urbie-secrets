e36666dd5715c022e4dc86fdfb9ea1e171ef5cda
Dakaina <Dakaina@hotmail.com>
2024-04-23 15:45:29 +0200

    file unpacker in utility

--- src/main/java/nl/hu/cisq2/hupol/candidates/application/CandidateService.java
+++ src/main/java/nl/hu/cisq2/hupol/candidates/application/CandidateService.java
@@ -3,13 +3,12 @@ package nl.hu.cisq2.hupol.candidates.application;
 import jakarta.transaction.Transactional;
 import nl.hu.cisq2.hupol.candidates.data.CandidateRepository;
 import nl.hu.cisq2.hupol.candidates.domain.Candidate;
+import nl.hu.cisq2.hupol.utility.FileUnpacker;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-import java.util.List;
 
 @Service
 @Transactional
@@ -21,19 +20,13 @@ public class CandidateService {
     }
 
     public void importCandidates(MultipartFile file) throws IOException {
-        List<Candidate> candidates = new ArrayList<>();
-        String fileStringified = new String(file.getBytes(), StandardCharsets.UTF_8);
-        String[] rows = fileStringified.split("\r\n|\r|\n");
+        ArrayList<String[]> columnsList = FileUnpacker.getColumns(file);
 
-        for (int row = 0; row < rows.length; row++) {
-            if (!rows[row].isBlank()) {
-                String[] columns = rows[row].split(";");
-                candidates.add(new Candidate(columns[0], Long.parseLong(columns[1]), columns[2], columns[3]));
+        for (String[] columns : columnsList){
+            Candidate candidate = new Candidate(columns[0], Long.parseLong(columns[1]), columns[2], columns[3]);
+            if (!candidateRepository.existsById(candidate.getCandidateId())){
+                candidateRepository.save(candidate);
             }
-        }
-
-        for (int i = 0; i < candidates.size(); i++) {
-            candidateRepository.save(candidates.get(i));
         }
     }
 }
--- src/main/java/nl/hu/cisq2/hupol/votes/application/VoteService.java
+++ src/main/java/nl/hu/cisq2/hupol/votes/application/VoteService.java
@@ -1,16 +1,15 @@
 package nl.hu.cisq2.hupol.votes.application;
 
 import jakarta.transaction.Transactional;
+import nl.hu.cisq2.hupol.utility.FileUnpacker;
 import nl.hu.cisq2.hupol.votes.data.VoteRepository;
 import nl.hu.cisq2.hupol.votes.domain.Vote;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.List;
 
 @Service
 @Transactional
@@ -21,24 +20,14 @@ public class VoteService {
         this.voteRepository = voteRepository;
     }
 
-    public void importVotes(MultipartFile f) throws IOException {
-        List<Vote> vs = new ArrayList<>();
-        String c = new String(f.getBytes(), StandardCharsets.UTF_8);
-        String[] rs = c.split("\r\n|\r|\n");
+    public void importVotes(MultipartFile file) throws IOException {
+        ArrayList<String[]> columnsList = FileUnpacker.getColumns(file);
 
-        for (int rnum = 0; rnum < rs.length; rnum++) {
-            if(!rs[rnum].isEmpty()){
-                if(!rs[rnum].isBlank()&&rs[rnum].length()>0){
-                    String[] cols = rs[rnum].split(";");
-                    vs.add(new Vote(Long.parseLong(cols[0]), Long.parseLong(cols[1]), cols[2], LocalDate.parse(cols[3]), cols[4]));                        } else {
-                    continue;
-                }
-            } else {
-                continue;
+        for (String[] columns : columnsList){
+            Vote vote = new Vote(Long.parseLong(columns[0]), Long.parseLong(columns[1]), columns[2], LocalDate.parse(columns[3]), columns[4]);
+            if (!voteRepository.existsById(vote.getVoteId())){
+                voteRepository.save(vote);
             }
-        }
-        for (int i = 0; i < vs.size(); i++) {
-            if (!voteRepository.existsById(vs.get(i).getVoteId())) voteRepository.save(vs.get(i));
         }
     }
 }
