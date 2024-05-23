c3e832425d5d7f13a7b265421c8efb88ad1f6b42
Dakaina <Dakaina@hotmail.com>
2024-05-23 20:33:42 +0200

    @pattern regex (al mijn andere code voor niks)

--- src/main/java/nl/hu/cisq2/hupol/security/application/exception/PasswordRequirementsNotMet.java
+++
@@ -1,9 +0,0 @@
-package nl.hu.cisq2.hupol.security.application.exception;
-
-public class PasswordRequirementsNotMet extends RuntimeException {
-    public PasswordRequirementsNotMet(){
-        super("Password should have 12 characters existing out of at least 2 uppercase letter, 2 lowercase letters, 2 numbers," +
-                " 2 special characters (~`!@#$%^&*()_-+=[]{}|:;<>,./?')");
-    }
-}
-
--- src/main/java/nl/hu/cisq2/hupol/security/presentation/dto/RegisterRequest.java
+++ src/main/java/nl/hu/cisq2/hupol/security/presentation/dto/RegisterRequest.java
@@ -1,12 +1,15 @@
 package nl.hu.cisq2.hupol.security.presentation.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public record RegisterRequest(
         @NotBlank
         String username,
 
-        @NotBlank
+        @Size(min = 12)
+        @Pattern(regexp = "^(?=.*\\d{2})(?=.*[a-z]{2})(?=.*[A-Z]{2})(?=.*\\p{P}{2}).*$" , message = "Password should be at least 12 characters and include at least 2 lowercase letters, 2 uppercase letters, 2 digits and 2 punctuation marks")
         String password
 ) {
 }
